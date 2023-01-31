a407905a46b77b321bed76b962dc322d37ed976a
Arshad Khan <arshad.k@zebpay.com>
2023-01-31 21:10:31 +0530

    enhancement and refactor

---
+++ components/UserHome/Contract/context.ts
@@ -0,0 +1,4 @@
+export type ContractContextType = {
+    addContract: (category: string, type: string, description: string, name: string, email: string, startDate: string, endDate: string,createDate: string, sha256: string, IPFSUri: string)  => {};
+    getContract: (IPFSUri: string)  => any;
+};
--- components/UserHome/UserDocuments/index.tsx
+++ components/UserHome/UserDocuments/index.tsx
@@ -10,7 +10,7 @@ import * as styles from "./styles";
 import { NFT_TOKEN } from "../../../constants/constants";
 import { blobToSHA256 } from "file-to-sha256";
 import { MoreOutlined } from "@ant-design/icons";
-import { ContractContextType } from "../../../context";
+import { ContractContextType } from "./../Contract/context";
 import { contractContext } from "./../Contract";
 
 const UserDocuments = () => {
@@ -52,14 +52,18 @@ const UserDocuments = () => {
         console.log(metadata);
         const sha256 = await blobToSHA256(uplodedDocument);
         const currentTime = new Date();
+        //@TODO should get values from the form (remove hardcoded strings)
         addContract(
-          values.category,
+          values.category || "category",
+          "type",
           description,
-          name,
-          values.email,
+          name || "name",
+          values.email || "email",
+          "start_date",
+          "end_date",
           currentTime.toLocaleString(),
           sha256,
-          "metadata"
+          "metadata",
         );
         //upload details to Blockchain
       }

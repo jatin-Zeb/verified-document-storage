99097753a92e7f7702706150f19cd0aeacea8d29
Jatin Sonkusale <jatin.s@zebpay.com>
2023-03-15 17:21:40 +0530

    bug fixed

--- pages/aboutUs.tsx
+++ pages/aboutUs.tsx
@@ -1,21 +1,11 @@
 /** @jsxImportSource @emotion/react */
 
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
-
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import SideBar from "../components/SideBar";
-import UserHome from "../components/UserHome";
 
 const About: NextPage = () => {
-  const [showKycFlow, setShowKycFlow] = useState<boolean>(false);
-  const [openSideDrawer, setOpenSideDrawer] = useState<boolean>(false);
 
-  useEffect(() => {
-    //call user api to check if logged in
-    //call api for stored document when user is successful
-  });
   return (
     <div>
       <div
@@ -28,7 +18,6 @@ const About: NextPage = () => {
         <div css={{ display: "flex" }}>
           <div
             css={{
-              width: openSideDrawer ? "90%" : "95%",
               justifyContent: "end",
               transition: "0.2s all ease-in",
               margin: "auto auto",
